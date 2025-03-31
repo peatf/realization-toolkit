1120956b28cf839134c6cff5a333fcf90872b392
peatf <peathefeary@gmail.com>
2025-03-30 23:51:47 -0700

    Update and rename QuizSection.tsx to QuizSectionMock.tsx

--- components/sections/QuizSection.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// This is a placeholder file - the actual implementation is done via the mock component in index.tsx
-const QuizSection: React.FC = () => {
-  return null;
-};
-
-export default QuizSection;
