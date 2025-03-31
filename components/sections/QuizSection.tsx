c637d9d98533e6d68ef14a500714fb688374120e
peatf <peathefeary@gmail.com>
2025-03-30 21:29:55 -0700

    Update QuizSection.tsx

---
+++ components/sections/QuizSection.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// This is a placeholder file - the actual implementation is done via the mock component in index.tsx
+const QuizSection: React.FC = () => {
+  return null;
+};
+
+export default QuizSection;
