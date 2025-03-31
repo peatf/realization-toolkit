1001891a5646b50695163886057ce1e89c5ed24d
peatf <peathefeary@gmail.com>
2025-03-30 23:52:18 -0700

    Update and rename ToolShowcases.tsx to ToolShowcasesMock.tsx

--- components/sections/ToolShowcases.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// This is a placeholder file - the actual implementation is done via the mock component in index.tsx
-const ToolShowcases: React.FC = () => {
-  return null;
-};
-
-export default ToolShowcases;
