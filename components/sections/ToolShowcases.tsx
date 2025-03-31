c0b6c264d60a67a2f7cf33ccff7260c95fa3c6ab
peatf <peathefeary@gmail.com>
2025-03-30 21:30:16 -0700

    Update ToolShowcases.tsx

---
+++ components/sections/ToolShowcases.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// This is a placeholder file - the actual implementation is done via the mock component in index.tsx
+const ToolShowcases: React.FC = () => {
+  return null;
+};
+
+export default ToolShowcases;
